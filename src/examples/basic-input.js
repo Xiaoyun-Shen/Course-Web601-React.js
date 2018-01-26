6911abe54ff5495cff579b70645995b97ec5c0c5
Xiaoyun Shen <xiaoyunshenpitt@gmail.com>
2018-01-26 08:56:06 -0500

    updated basic-input for assignment01

--- src/examples/basic-input.js
+++ src/examples/basic-input.js
@@ -2,24 +2,41 @@
 class BasicInput extends React.Component {
   constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
+    //this.handleChange = this.handleChange.bind(this);
+	this.handleClick = this.handleClick.bind(this);
     this.state = {
-      userInput: ''
+      userInput: '',
+	  lists:[]
     };
   }
-  handleChange(e) {
-    this.setState({userInput: e.target.value});
+  //handleChange(e) {
+  //  this.setState({userInput: e.target.value});
+  //}onChange={this.handleChange}
+  
+  handleClick(e){
+	 //this.setState({userInput:  document.getElementById('input').value}); 
+	 let newValue=document.getElementById('input').value;
+	 this.setState({lists:  [...this.state.lists, newValue]}); 
+	 document.getElementById('input').value='';
   }
+	  
   render() {
+	const list = this.state.lists.map((item, i) => {
+		return <li key={i}>{item}</li>
+	});
     return (
       <div>
           <p>
             Please enter some input
           </p>
-          <input onChange={this.handleChange}/>
-          <p>
-            You entered: {this.state.userInput}
-          </p>
+          <input id="input" />
+		  <button onClick={this.handleClick}>Add</button>
+         
+		  
+		  <ul>
+			{list}
+		  </ul>
+		  
       </div>
     ); 
   }
